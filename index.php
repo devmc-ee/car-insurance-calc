055bb6b8ffc56474a6cb156b8588f6d9e84e1b3b
Aleks Devmcee <info@devmc.ee>
2020-10-06 21:00:20 +0300

    init

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$request = $_SERVER['REQUEST_URI'];
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Insly-Test</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<nav>
+    <ul class="tasks-menu">
+        <li class="tasks-menu__item"><a href="task1">Task 1</a></li>
+        <li class="tasks-menu__item"><a href="task2">Task 2</a></li>
+        <li class="tasks-menu__item"><a href="Task3">Task 3</a></li>
+    </ul>
+</nav>
+<div class="task-container">
+    <div class="task-wrapper">
+		<?php
+
+		if ('/' === $request) {
+			echo 'Please select the task';
+		} else {
+			//simple routing
+			include 'views/' . $request . '.php';
+		}
+		?>
+    </div>
+</div>
+</body>
+</html>
+
---
+++ views/task1.php
@@ -0,0 +1,6 @@
+<?php ?>
+<h1>Task 1: Name</h1>
+<div class="task__text">
+	print out your name with one of php loops
+</div>
+<hr>
