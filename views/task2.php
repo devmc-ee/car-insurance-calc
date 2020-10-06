a23a3c9b724202d8f7679e6ad8dfc0c221a10f5b
Aleks Devmcee <info@devmc.ee>
2020-10-07 00:12:56 +0300

    add styles to the calculator form

--- views/task2.php
+++ views/task2.php
@@ -3,36 +3,39 @@
     Write a simple car insurance calculator which will output price of the policy using vanilla PHP and JavaScript:
 </div>
 <hr>
-<h3 class="task-intro">Calculator</h3>
+
 <div class="task-result">
+    <div class="task-form__wrapper">
+        <h3 class="task-intro">Calculator</h3>
+        <!--1. Create HTML form with fields:
+		• Estimated value of the car (100 - 100 000 EUR)
+		• Tax percentage (0 - 100%)
+		• Number of instalments (count of payments in which client wants to pay for the policy (1 – 12))
+		• Calculate button-->
 
-    <!--1. Create HTML form with fields:
-	• Estimated value of the car (100 - 100 000 EUR)
-	• Tax percentage (0 - 100%)
-	• Number of instalments (count of payments in which client wants to pay for the policy (1 – 12))
-	• Calculate button-->
+        <form id="calculator" class="calculator-form" action="tasks_solutions/task2/form-action.php" method="post">
+            <label for="carValue">Estimated value of the car (100 - 100 000 EUR):</label>
+            <input id="carValue" name="carValue" type="number" min="100" max="100000" placeholder="100 - 100 000 EUR"
+                   value="">
 
-    <form id="calculator" class="calculator-form" action="" method="post">
-        <label for="carValue">Estimated value of the car (100 - 100 000 EUR):</label>
-        <input id="carValue" name="carValue" type="number" min="100" max="100000" placeholder="100 - 100 000 EUR"
-               value="">
+            <label for="taxPercentage">Tax percentage (0 - 100%):</label>
+            <input id="taxPercentage" name="taxPercentage" type="number" min="0" max="100" placeholder="0 - 100%"
+                   value="">
 
-        <label for="taxPercentage">Tax percentage (0 - 100%):</label>
-        <input id="taxPercentage" name="taxPercentage" type="number" min="0" max="100" placeholder="0 - 100%" value="">
+            <label for="instalmentsNumber"> Number of instalments (count of payments in which client wants to pay for
+                the
+                policy (1 – 12))</label>
+            <select name="instalmentsNumber" id="instalmentsNumber">
+                <option value=""></option>
+				<?php for ($i = 1; $i <= 12; $i++) : ?>
+                    <option value="<?php echo $i; ?>">
+						<?php echo $i; ?>
+                    </option>
+				<?php endfor; ?>
+            </select>
 
-        <label for="instalmentsNumber"> Number of instalments (count of payments in which client wants to pay for the
-            policy (1 – 12))</label>
-        <select name="instalmentsNumber" id="instalmentsNumber">
-            <option value=""></option>
-			<?php for ($i = 1; $i <= 12; $i++) : ?>
-                <option value="<?php echo $i; ?>">
-					<?php echo $i; ?>
-                </option>
-			<?php endfor; ?>
-        </select>
-
-        <button type="submit">CALCULATE</button>
-    </form>
-
+            <button type="submit">CALCULATE</button>
+        </form>
+    </div>
 </div>
 <hr>
